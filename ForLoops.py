58431f113f197a8128e6ccd6f4d5b5ed46876c48
Eric Jordan <ericjames1495@gmail.com>
2017-12-12 16:50:06 +0000

    Adding extra metrics

--- ForLoops.py
+++ ForLoops.py
@@ -5,12 +5,13 @@ from sklearn.metrics import mean_squared_error, r2_score
 import pandas as pd
 import scipy as sp
 from sklearn.metrics import mean_squared_log_error, make_scorer
-from sklearn.metrics import mean_absolute_error, precision_score
+from sklearn.metrics import mean_absolute_error, precision_score, f1_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn import preprocessing, neighbors
 from sklearn.metrics import accuracy_score
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
+import warnings
 
 def normaliseScores(scores):
     old_max = max(scores)
@@ -54,9 +55,22 @@ def executeAlgorithms(X, y):
     return
 
 def metricEvaluation(model, X, y):
-    accuracy = cross_val_score(model, X, y, cv=10, scoring="accuracy")
-    # precision_scorer = make_scorer(precision_score, average="weighted")
-    # precision = cross_val_score(model, X, y, cv=10, scoring=precision_scorer)
+    accuracy_result = cross_val_score(model, X, y, cv=10, scoring="accuracy")
+    f1_weighted_result = cross_val_score(model, X, y, cv=10, scoring="f1_weighted")
+    f1_micro_result = cross_val_score(model, X, y, cv=10, scoring="f1_micro")
+    f1_macro_result = cross_val_score(model, X, y, cv=10, scoring="f1_macro")
+    # f1_samples_result = cross_val_score(model, X, y, cv=10, scoring="f1_samples")
+    precision_weighted_result = cross_val_score(model, X, y, cv=10, scoring="precision_weighted")
+    # average_precision_result = cross_val_score(model, X, y, cv=10, scoring="average_precision")
+    # TODO Replace precision with F1_score
+    # f1_scorer = make_scorer(f1_score, average="weighted")
+    # 'f1_score' is not a valid scoring value. Valid options are ['accuracy', 'adjusted_mutual_info_score',
+    # 'adjusted_rand_score', 'average_precision', 'completeness_score', 'explained_variance', 'f1', 'f1_macro',
+    # 'f1_micro', 'f1_samples', 'f1_weighted', 'fowlkes_mallows_score', 'homogeneity_score', 'mutual_info_score',
+    # 'neg_log_loss', 'neg_mean_absolute_error', 'neg_mean_squared_error', 'neg_mean_squared_log_error',
+    # 'neg_median_absolute_error', 'normalized_mutual_info_score', 'precision', 'precision_macro', 'precision_micro',
+    # 'precision_samples', 'precision_weighted', 'r2', 'recall', 'recall_macro', 'recall_micro', 'recall_samples',
+    #  'recall_weighted', 'roc_auc', 'v_measure_score']
     # RMS Error
     # mean_squared_error = cross_val_score(model, X, y, cv=10, scoring="neg_mean_squared_error") * -1
     # mean_squared_error = normaliseScores(mean_squared_error)
@@ -80,8 +94,13 @@ def metricEvaluation(model, X, y):
     # start_time = time.time()
     # cross_val_score(model, X, y, cv=10)
     # runtime = time.time() - start_time
-    print("Accuracy                         =", accuracy.mean(),"%")
-    # print("Precision                        =", precision.mean(),"%")
+    print("Accuracy                         =", accuracy_result.mean(), "%")
+    print("F1_weighted                      =", f1_weighted_result.mean(),"%")
+    print("Precision_weighted               =", precision_weighted_result.mean(),"%")
+    print("F1_micro                         =", f1_micro_result.mean(),"%")
+    print("F1_macro                         =", f1_macro_result.mean(),"%")
+    # print("F1_samples                      =", f1_samples_result.mean(),"%")
+    # print("Precision_average                      =", average_precision_result.mean(),"%")
     # print("Median absolute error             =", median_absolute_error.mean())
     # print("R2 score                          =", r2_score.mean())
     # print("Absolute mean error               =", abs_mean_error.mean())
@@ -92,13 +111,16 @@ def metricEvaluation(model, X, y):
 
     return
 
+### Added to suppress warnings for ill-defined precision, should be removed if other issues arise
+warnings.filterwarnings("ignore")
+
 Features = ['fixed acidity','volatile acidity','citric acid','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','pH','sulphates','alcohol']
 #
 # whiteWine = pd.read_csv("~/Desktop/Python/ML3/", sep=";")
 # redWine = pd.read_csv("/Users/markloughman/Desktop/winequality-white.csv", sep=";")
 
 print("----- winequality-red -----")
-dataframe = pd.read_csv("/Users/markloughman/Desktop/winequality-red.csv", sep=";")
+dataframe = pd.read_csv("/home/eric/Desktop/4th Year/MachineLearning/Assignment3/Wine/winequality-red.csv", sep=";")
 X = dataframe.loc[:, Features]
 y = dataframe.quality
 
